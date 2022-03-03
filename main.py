da01656e21ba7341f41cf808a313b2534b380f45
Marcela Mosiño <68758494+MarcelaMosino@users.noreply.github.com>
2022-03-02 21:29:17 -0600

    Definir punto inicial aleatorio
    Primer intento

--- main.py
+++ main.py
@@ -5,13 +5,30 @@ from sympy import (
     Matrix,
     parse_expr
 )
+import random
+
+def find_rand(f, variables):  ###FUNCION QUE DEVUELVE LOS VALORES DE LAS VARIABLES
+    t = ""+str(f)               ##PARA EL PUNTO INICIAL
+    valores = []
+    #print(t)
+    for s in range(len(variables)-1):
+        r = random.randint(-5,5)
+        valores.append(r)
+        t = t.replace(variables[s],str(r))
+    #print(t)
+    t = sympy.sympify(t)
+    last = sympy.solve(t,variables[y])
+    valores.append(last)
+
+    return valores
 
 
 def find_max(f, syms, h: float = 1e-3, eps: float = 1e-3, iterations: int = 100):
     variables = symbols(syms)
     grad_f = lambdify(variables, derive_by_array(f, variables))
     result = Matrix([1] * len(variables))
-
+    
+    find_rand(f, syms) #Linea de prueba
     for _ in range(iterations):
         grad_value = Matrix(grad_f(*result))
 
